eed95d70f5dd0c1160ccbe397bd347f9abb314c9
Juan Jose <50469522+JuanJose2019@users.noreply.github.com>
2025-04-04 22:07:07 -0600

    Proyecto

---
+++ Administracion/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+using Administracion.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Administracion.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Reporte()
+        {
+            return View();
+        }
+
+        public IActionResult VentasReporte()
+        {
+            return View();
+        }
+
+        public IActionResult Buscar()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
